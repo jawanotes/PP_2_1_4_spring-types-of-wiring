e8c2a47eac3a95ec2d8023f75cb105787ba9ac22
hkl <hkl@local>
2023-01-24 19:53:01 +0500

    Test passed

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,19 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    /***
+     * Это работает
+     * Для Wood3 аннотацию @Autowired тоже можно закомментировать
+     */
+/*    @Autowired
+    public Deth8() {
+    }*/
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
